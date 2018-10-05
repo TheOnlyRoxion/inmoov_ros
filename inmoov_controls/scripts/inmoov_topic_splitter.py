01d5f16814e78ed34b27dcf61a7e7d367014b541
nixdorf <roxion@tobiasnixdorf.de>
2018-10-05 17:48:04 +0200

    Updated skript to coding convention

--- inmoov_controls/scripts/inmoov_topic_splitter.py
+++ inmoov_controls/scripts/inmoov_topic_splitter.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # licensed under BSD-3
 
-import rospy
 import time
+import rospy
 from sensor_msgs.msg import JointState
 from inmoov_msgs.msg import MotorCommand
 
@@ -10,68 +10,68 @@ pub_r = rospy.Publisher('/joint_states_r', MotorCommand, queue_size=10)
 pub_l = rospy.Publisher('/joint_states_l', MotorCommand, queue_size=10)
 
 
-name=()
-servoPin=()
-minGoal=()
-maxGoal=()
-minIn=()
-maxIn=()
-bus=()
+NAME = ()
+SERVO_PIN = ()
+MIN_GOAL = ()
+MAX_GOAL = ()
+MIN_IN = ()
+MAX_IN = ()
+BUS = ()
 
 
 def init():
-    global name
-    global servoPin
-    global minGoal
-    global maxGoal
-    global minIn
-    global maxIn
-    global bus
+    global NAME
+    global SERVO_PIN
+    global MIN_GOAL
+    global MAX_GOAL
+    global MIN_IN
+    global MAX_IN
+    global BUS
     time.sleep(1)
     for j,b in rospy.get_param('/joints').items():
-        name+=j,
-        servoPin+=rospy.get_param('/joints/' + j + '/servoPin',99),
-        minGoal+=rospy.get_param('/joints/' + j + '/minGoal',40),
-        maxGoal+=rospy.get_param('/joints/' + j + '/maxGoal',40),
-        bus+=rospy.get_param('/joints/' + j + '/bus',0),
-        minIn+=rospy.get_param('/robot_description_planning/joint_limits/' + j + '/min_position',0),
-        maxIn+=rospy.get_param('/robot_description_planning/joint_limits/' + j + '/max_position',0),
-    #rospy.loginfo("I found joints: %s with pin %s with input range %s / %s and output range %s / %s", name,servoPin,minIn,maxIn,minGoal,maxGoal)
- 
- 
-def mapFromTo(x,a,b,c,d):
-   y=(x-a)/(b-a)*(d-c)+c
-   return y
+        NAME += j,
+        SERVO_PIN += rospy.get_param('/joints/' + j + '/servoPin', 99),
+        MIN_GOAL += rospy.get_param('/joints/' + j + '/minGoal', 40),
+        MAX_GOAL += rospy.get_param('/joints/' + j + '/maxGoal', 40),
+        BUS += rospy.get_param('/joints/' + j + '/bus', 0),
+        MIN_IN += rospy.get_param('/robot_description_planning/joint_limits/' + j + '/min_position', 0),
+        MAX_IN += rospy.get_param('/robot_description_planning/joint_limits/' + j + '/max_position', 0),
+#rospy.loginfo("I found joints: %s with pin %s with input range %s / %s and output range %s / %s", name,servoPin,minIn,maxIn,minGoal,maxGoal)
+
+
+def map_from_to(x,a,b,c,d):
+    y = (x-a)/(b-a)*(d-c)+c
+    return y
 
 
 def transform_callback(data):
-    global name
-    global servoPin
-    global minGoal
-    global maxGoal
-    global minIn
-    global maxIn
-    global bus
-    
-    motorcommand = MotorCommand()  
-    
-    for i,n in enumerate(name,0):
+    """here the magic happens"""
+    global NAME
+    global SERVO_PIN
+    global MIN_GOAL
+    global MAX_GOAL
+    global MIN_IN
+    global MAX_IN
+    global BUS
+
+    motorcommand = MotorCommand()
+
+    for i, n in enumerate(NAME, 0):
         if n in data.name :
-            if servoPin[i]<90:
-                if bus[i]==1:
-                    motorcommand.id = servoPin[i]
-                    motorcommand.value = mapFromTo(data.position[data.name.index(n)], minIn[i],maxIn[i], minGoal[i], maxGoal[i])
+            if SERVO_PIN[i]<90:
+                if BUS[i]==1:
+                    motorcommand.id = SERVO_PIN[i]
+                    motorcommand.value = map_from_to(data.position[data.name.index(n)], MIN_IN[i],MAX_IN[i], MIN_GOAL[i], MAX_GOAL[i])
                     pub_r.publish(motorcommand)
-                if bus[i]==2:
-                    motorcommand.id = servoPin[i]
-                    motorcommand.value = mapFromTo(data.position[data.name.index(n)], minIn[i],maxIn[i], minGoal[i], maxGoal[i])
+                if BUS[i]==2:
+                    motorcommand.id = SERVO_PIN[i]
+                    motorcommand.value = map_from_to(data.position[data.name.index(n)], MIN_IN[i],MAX_IN[i], MIN_GOAL[i], MAX_GOAL[i])
                     pub_l.publish(motorcommand)
                 #rospy.loginfo("I move %s at Pin %s  to %s",n,motorcommand.id, motorcommand.value)
 
-        
-
 
 def inmoov_splitter():
+    """init_node"""
     rospy.init_node('inmoov_splitter', anonymous=True)
     rospy.Subscriber('joint_states', JointState, transform_callback)
     rospy.spin()
@@ -82,8 +82,4 @@ if __name__ == '__main__':
         init()
         inmoov_splitter()
     except rospy.ROSInterruptException:
-        pass  
-    
-    
-    
-   
+        pass
